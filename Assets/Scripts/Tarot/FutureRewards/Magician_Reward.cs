cf226b7994709aec675161efcb9008d2266704e9
summerxa <71350144+summerxa@users.noreply.github.com>
2025-11-13 12:04:20 -0500

    Change Magician_Reward CD and use count

--- Assets/Scripts/Tarot/FutureRewards/Magician_Reward.cs
+++ Assets/Scripts/Tarot/FutureRewards/Magician_Reward.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Magician_Reward : Future_Reward
 {
-    public Magician_Reward(Future_TarotCard card) : base(1, 0f, card)
+    public Magician_Reward(Future_TarotCard card) : base(3, 10f, card)
     {
     }
 

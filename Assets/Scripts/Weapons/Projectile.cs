2e726174217ae25746b2ffbedd4d28963719b838
penguin3145 <ryananam3145@gmail.com>
2025-11-29 23:26:38 -0500

    2d projectiles

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,51 +1,37 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 public class Projectile : MonoBehaviour
 {
     private Vector3 firingPoint;
-
     [SerializeField]
     private float projectileSpeed;
-
     [SerializeField]
     private float maxProjectileDistance;
-
     public static event Action<EnemyBase> OnEnemyHit;
-
     private int bulletBounces;
-
     private Effects bulletEffect;
-
     private int bulletPiercing;
-
     private int enemiesPierced;
-
     public void setBulletBounce(int n)
     {
         bulletBounces = n;
     }
-
     public void setBulletPiercing(int n) { bulletPiercing = n; }
-
     public void setBulletEffect(Effects bulletEffect)
     {
         this.bulletEffect = bulletEffect;
     }
-
     void Start()
     {
         firingPoint = transform.position;
         enemiesPierced = 0;
     }
-
     // Update is called once per frame
     void Update()
     {
         MoveProjectile();
     }
-
     void MoveProjectile()
     {
         if (Vector3.Distance(firingPoint, transform.position) > maxProjectileDistance)
@@ -57,12 +43,10 @@ public class Projectile : MonoBehaviour
             transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
         }
     }
-
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-
         }
         else if (other.gameObject.GetComponent<EnemyBase>())
         {
@@ -71,8 +55,9 @@ public class Projectile : MonoBehaviour
             // TODO change to DamageType.Spiritual if player is dealing spiritual damage
             enemy.TakeDamage(PlayerController.instance.playerAttributes.damage,
                 DamageInstance.DamageSource.Player, DamageInstance.DamageType.Basic);
-            if (enemy.GetComponent<EffectManager>()) {
-                if(!(bulletEffect == null))
+            if (enemy.GetComponent<EffectManager>())
+            {
+                if (!(bulletEffect == null))
                     enemy.GetComponent<EffectManager>().AddEffect(bulletEffect);
             }
             enemiesPierced++;
@@ -86,11 +71,10 @@ public class Projectile : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("collision");
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
             Debug.Log("collision detected");
             if (bulletBounces > 0)

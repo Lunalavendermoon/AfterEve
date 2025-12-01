3b3d5c33f5b308d5cfbc1499b2f02bf924f23870
soyaoh <139798714+soyaoh@users.noreply.github.com>
2025-11-30 16:05:47 -0800

    Revert Present to 6809fb0

--- Assets/Scripts/Tarot/Present/HighPriestess_Present.cs
+++ Assets/Scripts/Tarot/Present/HighPriestess_Present.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class HighPriestess_Present : Present_TarotCard
 {
+    float[] applyProbability = {.25f, .3f, .4f, .5f, .6f};
+    float[] spiritualApplyProbability = {.5f, .6f, .7f, .8f, 1};
+    float weakPercent = .2f;
+    
+    PlayerGun playerGun;
+
     public HighPriestess_Present(int q) : base(q)
     {
         name = "HighPriestess_Present";
+
+        // TODO make effect only apply sometimes
+        playerGun = PlayerController.instance.gameObject.GetComponent<PlayerGun>();
+        playerGun.AddEffect(new Weak_Effect(3, weakPercent));
     }
+
 }
--- Assets/Scripts/Tarot/Present/Magician_Present.cs
+++ Assets/Scripts/Tarot/Present/Magician_Present.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Magician_Present : Present_TarotCard
 {
+    int[] bounceNum = {3,3,4,4,5};
+    float[] damageReducedPerBounce = {.3f, .25f, .2f, .1f, 0f};
+    
     public Magician_Present(int q) : base(q)
     {
+        PlayerController.instance.playerAttributes.bulletBounces = bounceNum[level];
+        PlayerController.instance.playerAttributes.bulletBounceDmgDecrease = damageReducedPerBounce[level];
         name = "Magician_Present";
     }
 }

de08dc47d16067acad720a078a648e0941ba6edc
summerxa <anne.xia6@gmail.com>
2025-12-24 20:15:28 -0800

    InteractableNPC supports multiple interactions

--- Assets/Scripts/Interactable/InteractableNPC.cs
+++ Assets/Scripts/Interactable/InteractableNPC.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 
+// represents an NPC with dialogue
 public class InteractableNPC : InteractableEntity
 {
+    public string nextInteractionNode;
+
+    protected bool firstInteraction = true;
+
     public override void TriggerInteraction()
     {
+        if (!firstInteraction && string.IsNullOrWhiteSpace(nextInteractionNode))
+        {
+            return;
+        }
+
+        string targetNode = firstInteraction ? yarnSpinnerNode : nextInteractionNode;
+
         PlayerController.instance.playerInput.Disable();
-        runner.StartDialogue(yarnSpinnerNode);
+        runner.StartDialogue(targetNode);
+        firstInteraction = false;
     }
 }

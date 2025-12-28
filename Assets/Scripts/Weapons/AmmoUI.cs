8c5e80103088f69694dfb593d28503805deeef46
Lunalavendermoon <lunalavendermoon@gmail.com>
2025-12-28 15:42:01 -0800

    Added + implemented attack direction indicator

--- Assets/Scripts/Weapons/AmmoUI.cs
+++ Assets/Scripts/Weapons/AmmoUI.cs
@@ -6,6 +6,10 @@ public class AmmoUI : MonoBehaviour
     public PlayerAttributes playerAttributes;
     [SerializeField] private Image[] bulletIcons;
 
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform attackDirectionIndicator;
+    [SerializeField] private Transform attackPivotCenter;
+
     //Singleton
     public static AmmoUI Instance;
 
@@ -17,6 +21,42 @@ public class AmmoUI : MonoBehaviour
         totalAmmo = playerAttributes.Ammo;
         //initializeAmmoUI(3); //Testing
     }
+    
+    public float radius = 2f;
+
+    void Start()
+    {
+        attackDirectionIndicator.transform.parent = attackPivotCenter;
+        attackDirectionIndicator.transform.position += Vector3.up * radius;
+    }
+
+    void Update()
+    {
+        // get rotation direction (as vector)
+        Vector3 targetAngleVector = Input.mousePosition - Camera.main.WorldToScreenPoint(player.transform.position);
+        float targetAngle = Mathf.Atan2(targetAngleVector.y, targetAngleVector.x) * Mathf.Rad2Deg;
+
+        // pivot.position = orb.position; //not necessary (?)
+        attackPivotCenter.rotation = Quaternion.AngleAxis(targetAngle - 90, Vector3.forward);
+    }
+
+    // void Update()
+    // {
+    //     Vector3 mouse_pos = Input.mousePosition;
+	//     Vector3 player_pos = Camera.main.WorldToScreenPoint(player.position);
+	//     mouse_pos.x = mouse_pos.x - player_pos.x;
+	//     mouse_pos.y = mouse_pos.y - player_pos.y;
+	    
+    //     float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+    //     Vector3 targetAngle = new Vector3(0, 0, angle);
+
+
+    //     // Quaternion targetAngle = Quaternion.Euler(new Vector3(0, 0, angle));
+    //     Debug.Log("Rotation angle: " + targetAngle);
+    //     // transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+
+    //     transform.RotateAround(attackPivotCenter.position, Vector3.up, 20 * Time.deltaTime);
+    // }
 
     public void initializeAmmoUI()
     {

13a7a9426cff9abada9c77c828794726bd4bc9e5
summerxa <anne.xia6@gmail.com>
2026-02-16 22:42:31 -0500

    more inventory ui

--- Assets/Scripts/UI/InventoryUIScript.cs
+++ Assets/Scripts/UI/InventoryUIScript.cs
@@ -4,13 +4,38 @@ public class InventoryUIScript : MonoBehaviour
 {
     public GameObject canvas;
 
+    // 0 = past, 1 = present, 2 = future
+    int state;
+
     void Start()
     {
         SetInventoryDisplay(false);
+
+        state = 1;
     }
 
     public void SetInventoryDisplay(bool enabled)
     {
         canvas.SetActive(enabled);
+
+        if (enabled)
+        {
+            RefreshInventoryDisplay();
+        }
+    }
+
+    public void SwitchDisplay(int newState)
+    {
+        if (newState != state)
+        {
+            state = newState;
+            
+            RefreshInventoryDisplay();
+        }
+    }
+
+    void RefreshInventoryDisplay()
+    {
+        Debug.Log($"Set card display to state: {state}");
     }
 }

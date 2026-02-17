16906705ded3f17821c75c0aa4f16d4633d059bc
summerxa <anne.xia6@gmail.com>
2026-02-17 14:48:28 -0500

    Refactor inventory to use premade tarot sprite SO

--- Assets/Scripts/UI/Inventory/InventoryItemUI.cs
+++ Assets/Scripts/UI/Inventory/InventoryItemUI.cs
@@ -5,8 +5,7 @@ public class InventoryItemUI : MonoBehaviour
 {
     public Image image;
 
-    public TarotSpriteSO presentCards;
-    public TarotSpriteSO futureCards;
+    public TarotIcon icons;
 
     TarotCard card;
 
@@ -18,17 +17,7 @@ public class InventoryItemUI : MonoBehaviour
         uiScript = script;
 
         Sprite sprite = GetSprite();
-
-        if (sprite != null)
-        {
-            image.sprite = sprite;
-        }
-        else
-        {
-            // TODO for debug only
-            image.sprite = presentCards.fool;
-            Debug.LogWarning($"No sprite for tarot card {card}");
-        }
+        image.sprite = sprite;
 
         if (setSidebar)
         {
@@ -43,68 +32,19 @@ public class InventoryItemUI : MonoBehaviour
 
     Sprite GetSprite()
     {
-        TarotSpriteSO spriteSet;
-        if (card is Present_TarotCard)
+        TarotIcon.TarotType type;
+        if (card is Past_TarotCard)
         {
-            spriteSet = presentCards;
+            type = TarotIcon.TarotType.Past;
         }
-        else if (card is Future_TarotCard)
+        else if (card is Present_TarotCard)
         {
-            spriteSet = futureCards;
+            type = TarotIcon.TarotType.Present;
         }
         else
         {
-            // TODO add past cards
-            return null;
+            type = TarotIcon.TarotType.Future;
         }
-
-        switch (card.arcana)
-        {
-            case TarotCard.Arcana.Fool:
-                return spriteSet.fool;
-            case TarotCard.Arcana.Magician:
-                return spriteSet.magician;
-            case TarotCard.Arcana.HighPriestess:
-                return spriteSet.highPriestess;
-            case TarotCard.Arcana.Empress:
-                return spriteSet.empress;
-            case TarotCard.Arcana.Emperor:
-                return spriteSet.emperor;
-            case TarotCard.Arcana.Hierophant:
-                return spriteSet.hierophant;
-            case TarotCard.Arcana.Lovers:
-                return spriteSet.lovers;
-            case TarotCard.Arcana.Chariot:
-                return spriteSet.chariot;
-            case TarotCard.Arcana.Strength:
-                return spriteSet.strength;
-            case TarotCard.Arcana.Hermit:
-                return spriteSet.hermit;
-            case TarotCard.Arcana.WheelOfFortune:
-                return spriteSet.wheelOfFortune;
-            case TarotCard.Arcana.Justice:
-                return spriteSet.justice;
-            case TarotCard.Arcana.HangedMan:
-                return spriteSet.hangedMan;
-            case TarotCard.Arcana.Death:
-                return spriteSet.death;
-            case TarotCard.Arcana.Temperance:
-                return spriteSet.temperance;
-            case TarotCard.Arcana.Devil:
-                return spriteSet.devil;
-            case TarotCard.Arcana.Tower:
-                return spriteSet.tower;
-            case TarotCard.Arcana.Star:
-                return spriteSet.star;
-            case TarotCard.Arcana.Moon:
-                return spriteSet.moon;
-            case TarotCard.Arcana.Sun:
-                return spriteSet.sun;
-            case TarotCard.Arcana.Judgement:
-                return spriteSet.judgement;
-            case TarotCard.Arcana.World:
-                return spriteSet.world;
-        }
-        return null;
+        return icons.GetSprite(card.arcana, type);
     }
 }

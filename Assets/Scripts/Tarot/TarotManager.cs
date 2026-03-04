001ec5c55952755fbfa6de11ab2032cde9d83fe9
summerxa <anne.xia6@gmail.com>
2026-03-03 20:35:06 -0500

    Remove tarot cards from testing

--- Assets/Scripts/Tarot/TarotManager.cs
+++ Assets/Scripts/Tarot/TarotManager.cs
@@ -22,26 +22,8 @@ public class TarotManager : MonoBehaviour
         PlayerController.instance.playerInput.Player.UpdateTarotHand.performed += OnUpdateTarotPerformed; // for testing
         PlayerController.instance.playerInput.Player.Attack.performed += OnMouseClick;
 
-        // commented some cards out for testing purposes, uncomment if u need to use them :D
-
-        // AddCard(new Hierophant_Past(1));
-        // AddCard(new Fool_Past(1));
-        // AddCard(new Fool_Present(2));
-        // AddCard(new Empress_Present(1));
-
-        AddCard(new Chariot_Past(1));
-        AddCard(new Emperor_Past(1));
-        AddCard(new Empress_Past(1));
-        AddCard(new Hermit_Past(1));
-        AddCard(new Hierophant_Past(1));
-        AddCard(new HighPriestess_Past(1));
-        AddCard(new Lovers_Past(1));
-        AddCard(new Magician_Past(1));
-        AddCard(new Strength_Past(1));
-        AddCard(new Fool_Past(1));
-
         // TODO - uncomment this (only commented out for testing)
-        // DisplayHand();
+        DisplayHand();
     }
 
     void OnDisable()

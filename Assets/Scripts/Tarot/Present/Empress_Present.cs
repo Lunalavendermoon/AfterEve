ca944560b9168b45c6f64021546ffdc8417c3f55
summerxa <anne.xia6@gmail.com>
2026-03-28 15:29:52 -0700

    fix broken Empress present multipliers
    bruh

--- Assets/Scripts/Tarot/Present/Empress_Present.cs
+++ Assets/Scripts/Tarot/Present/Empress_Present.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Empress_Present : Present_TarotCard
 {
     float[] healProbability = {.2f, .25f, .3f, .25f, .4f};
-    float[] healPercent = {.5f, .7f, .9f, .11f, .13f};
+    float[] healPercent = {.05f, .07f, .09f, .11f, .13f};
 
     float timeBetweenTrigger = 1f;
 
